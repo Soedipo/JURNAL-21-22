73ca89679f146f199000d95c3014620c667c0d03
Soedipo <dipo.lendou@gmail.com>
2021-08-05 19:42:45 +0700

    Adding 6_4

---
+++ GANJIL/6_4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+// #include <stdlib.h>
+int main(){
+    int n, total=0, itemArr;
+    struct{
+        int harga;
+        char item[30];
+    }kasir[30];
+        
+    printf("---KASIR--- \n");
+    printf("Total Barang: "); scanf("%d", &n);
+
+// INPUT
+    for(itemArr=0 ; itemArr<n ; itemArr++){
+        getchar();
+        printf("%d. Nama Barang\t: ", itemArr+1);
+        gets(kasir[itemArr].item);
+        printf("   Harga\t: ");
+        scanf("%d", &kasir[itemArr].harga); 
+        printf("\n");
+    }
+
+// TOTAL HARGA
+    for(itemArr=0 ; itemArr<n ; itemArr++){
+        total += kasir[itemArr].harga;
+    }
+    
+    printf("\n"); 
+
+// OUTPUT
+    for(itemArr=0 ; itemArr<n ; itemArr++){
+        printf("%d. Nama Barang\t: %s\n", itemArr+1, kasir[itemArr].item);
+        printf("   Harga\t: %d\n\n", kasir[itemArr].harga);
+    }
+    printf("TOTAL HARGA\t: Rp%d", total);
+}
