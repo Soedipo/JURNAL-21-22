a3756b2f780649cbdc3e3ffa335e4522b29ef4ca
Soedipo <dipo.lendou@gmail.com>
2021-08-05 19:04:52 +0700

    Delete Det, change a to temp and edit input and output format

--- GANJIL/6_5.c
+++ GANJIL/6_5.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    int i, j, Det, a; float matriks[2][2];
+    int i, j, temp, matriks[2][2];
 
 //INPUT
     printf("Masukkan angka: \n");
     for(i=0 ; i<=1 ; i++){
         for(j=0 ; j<=1 ; j++){
-            printf("Matriks[%d][%d] = ", i+1, j+1);
-            scanf("%f", &matriks[i][j]);
+            printf("A[%d][%d] = ", i+1, j+1);
+            scanf("%d", &matriks[i][j]);
         }
     }
     system("cls");
 
 //OUTPUT MATRIKS 2x2 
-    printf("\nMATRIKS 2X2\n\n");
+    printf("\n---MATRIKS A---\n\n");
 
     for(i=0 ; i<=1 ; i++){
         for(j=0 ; j<=1 ; j++){
-            printf("    %.1f", matriks[i][j]);
+            printf("    %d", matriks[i][j]);
         }
         printf("\n\n");
     } 
 
 //Adj(M)
-    a = matriks[0][0];
+    temp = matriks[0][0];
     matriks[0][0] = matriks[1][1];
-    matriks[1][1] = a;
+    matriks[1][1] = temp;
     matriks[0][1] = - matriks[0][1]; matriks[1][0] = -matriks[1][0];  
 
 //OUTPUT Adj(M)
-    printf("ADJOIN\n\n");
+    printf("---Adj(A)---\n\n");
 for(i=0 ; i<=1 ; i++){
         for(j=0 ; j<=1 ; j++){
-            printf("    %.1f", matriks[i][j]);
+            printf("    %d", matriks[i][j]);
         }
         printf("\n\n");
     }    
